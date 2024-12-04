6a4e2873a017526bcbf414ca81e1f75196a3805d
Mateo071 <floresmateo2000@gmail.com>
2024-12-04 16:00:50 -0500

    working painting

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # import colorgram
+import turtle
 from turtle import Turtle, Screen
+from random import choice
+
+turtle.colormode(255)
 
 # colors = colorgram.extract('image.png', 9)
 
@@ -16,8 +20,29 @@ rgb_colors = [(151, 89, 49), (221, 143, 78), (47, 35, 27), (21, 24, 28), (189, 1
 
 # print(rgb_colors)
 
-dot = Turtle()
+def go_up():
+  pass
+def go_right():
+  pass
 
+def hirst():
+  dot = Turtle()
+  dot.up()
+  #go to bottom right corner to center painting
+  dot.goto(-250, -250)
+  #vertical
+  for _ in range(10):
+    for _ in range(10):
+      random_color = choice(rgb_colors)
+      dot.dot(20, random_color)
+      dot.forward(50)
+    dot.setheading(90)
+    dot.forward(50)
+    dot.setheading(180)
+    dot.forward(500)
+    dot.setheading(0)
+
+hirst()
 
 screen = Screen()
 screen.exitonclick()
