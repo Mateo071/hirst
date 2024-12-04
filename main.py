8d6a16f938e99b723da8896406f458d87f7f8904
Mateo071 <floresmateo2000@gmail.com>
2024-12-04 15:42:01 -0500

    used colorgram to find image colors

---
+++ main.py
@@ -0,0 +1,16 @@
+import colorgram
+
+colors = colorgram.extract('image.png', 9)
+
+rgb_colors = [(151, 89, 49), (221, 143, 78), (47, 35, 27), (21, 24, 28), (189, 134, 47), (215, 98, 55), (177, 225, 247), (240, 203, 104), (245, 217, 174)]
+# first_color = colors[0]
+
+# for color in colors:
+#   r = color.rgb.r
+#   g = color.rgb.g
+#   b = color.rgb.b
+#   new_color = (r, g, b)
+
+#   rgb_colors.append(new_color)
+
+# print(rgb_colors)
