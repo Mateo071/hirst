bc2dce8d37fe5448c26767b2f6d9f84be6d56b28
Mateo071 <floresmateo2000@gmail.com>
2024-12-04 15:43:52 -0500

    imported turtle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import colorgram
+# import colorgram
+from turtle import Turtle, Screen
 
-colors = colorgram.extract('image.png', 9)
+# colors = colorgram.extract('image.png', 9)
 
 rgb_colors = [(151, 89, 49), (221, 143, 78), (47, 35, 27), (21, 24, 28), (189, 134, 47), (215, 98, 55), (177, 225, 247), (240, 203, 104), (245, 217, 174)]
 # first_color = colors[0]
@@ -14,3 +15,9 @@ rgb_colors = [(151, 89, 49), (221, 143, 78), (47, 35, 27), (21, 24, 28), (189, 1
 #   rgb_colors.append(new_color)
 
 # print(rgb_colors)
+
+dot = Turtle()
+
+
+screen = Screen()
+screen.exitonclick()
